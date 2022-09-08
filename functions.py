c3243b5491b4b542221ba06266474588a6292d22
zxsama <844600516@qq.com>
2022-09-08 11:19:03 +0800

    更新自动安装

--- functions.py
+++ functions.py
@@ -6,9 +6,10 @@ def install_modul(self, *modul_name):
     import sys
     import subprocess
 
-    bpy.ops.wm.console_toggle()  # TODO: 判断控制台是否开启
+    # bpy.ops.wm.console_toggle()  # TODO: 判断控制台是否开启
+    python_path = os.path.join(sys.prefix, 'bin', 'python.exe')
+    print("Python Path: ", python_path)
     for modul in modul_name:
-        python_path = os.path.join(sys.prefix, 'bin', 'python.exe')
         print("-" * 30 + "\n未安装" + modul + "模块，安装中...\n")
         subprocess.call([python_path, "-m", "pip", "install", modul])
     print("\n安装结束.\n" + "重启blender后生效!\n" * 3 + "-" * 30)
