6a0e5559b4a18203fb98166417885a47a5b910f9
zxsama <844600516@qq.com>
2024-10-20 22:17:54 +0800

    旧版本兼容

--- cross_version_support.py
+++ cross_version_support.py
@@ -3,8 +3,8 @@ import bpy
 bl_version = bpy.app.version
 
 def language_zh_code():
-    if bl_version >= (4,0,0):
-        zh_code = "zh_HANS"
-    else:
+    if bl_version < (4,0,0):
         zh_code = "zh_CN"
+    else:
+        zh_code = "zh_HANS"
     return zh_code
