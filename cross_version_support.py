26d37fe6d5e4ecdde4cf8b17c0b741915e4949d1
zxsama <844600516@qq.com>
2024-10-20 22:14:08 +0800

    Revert "旧版本兼容"
    This reverts commit a6ee220e3a371b6f2c69c710a3b2ec81c51ed531.

--- cross_version_support.py
+++ cross_version_support.py
@@ -3,7 +3,7 @@ import bpy
 bl_version = bpy.app.version
 
 def language_zh_code():
-    if bl_version < (4,0,0):
+    if bl_version >= (4,0,0):
         zh_code = "zh_HANS"
     else:
         zh_code = "zh_CN"
