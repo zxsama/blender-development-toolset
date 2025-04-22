6aadcdf1f8f341573e0c1faa3dbefd8949348c3f
zxsama <844600516@qq.com>
2025-04-22 15:01:38 +0800

    重构console toggle

--- functions.py
+++ functions.py
@@ -5,21 +5,6 @@ import subprocess
 from bpy.types import bpy_prop_collection
 from .cross_version_support import VersionPlatformManager as VP_Manager
 
-
-def install_modul(self, *modul_name):
-    import os
-    import sys
-
-    # bpy.ops.wm.console_toggle()  # TODO: 判断控制台是否开启
-    python_path = os.path.join(sys.prefix, "bin", "python.exe")
-    print("Python Path: ", python_path)
-    for modul in modul_name:
-        print("-" * 30 + "\n未安装" + modul + "模块，安装中...\n")
-        subprocess.call([python_path, "-m", "pip", "install", "-I", modul])
-    print("\n安装结束.\n" + "重启blender后生效!\n" * 3 + "-" * 30)
-    self.report({"WARNING"}, "重启blender!")
-
-
 def collection_search(ID):
     def users(col):
         ret = tuple(repr(o) for o in col if o.user_of_id(ID))
